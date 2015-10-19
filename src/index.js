ae5b24410a3d034e201815c58e2360cffaee102a
Andrea Parodi <andrea@parro.it>
2015-10-19 20:17:12 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function test4() {
-  return Promise.resolve(42);
+const ShellComponent = require('./shell-component');
+
+module.exports = function init(app) {
+  app.config.on('all-preferences-loaded', () => {
+    const pref = app.packages.shell.preferences;
+    pref['user-css'] = (pref['user-css'] || '').replace(/~/g, app.config.configFolder);
+  });
+
+  app.on('packages-init-done', () => {
+    app.commands.execute('inject-script', __dirname + '/hterm_all.js');
+    setTimeout(()=>app.commands.execute('new-tab'), 100);
+  });
+
+  const pkg = {
+    initializeTerminal(t) {
+      Object.keys(this.preferences).forEach(k => {
+        t.prefs_.set(k, this.preferences[k]);
+      });
+    },
+
+    name: 'shell',
+    path: __dirname
+  };
+
+  app.commands.register('new-tab', () =>
+    app.tabs.add(new ShellComponent(app, pkg))
+  );
+
+
+  return pkg;
 };
